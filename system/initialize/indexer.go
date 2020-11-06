1a5c4f4b6684e27aadb6b4f995289eecb215ddd6
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-11-07 06:27:27 +0700

    system.initialize: add method OpenAllIndexerConnection

--- system/initialize/indexer.go
+++ system/initialize/indexer.go
@@ -1,11 +1,36 @@
 package initialize
 
 import (
+	"reflect"
+
 	"github.com/d3ta-go/system/system/config"
 	"github.com/d3ta-go/system/system/handler"
 	"github.com/d3ta-go/system/system/indexer"
 	"github.com/d3ta-go/system/system/indexer/adapter"
 )
+
+// OpenAllIndexerConnection open all indexer connection
+func OpenAllIndexerConnection(h *handler.Handler) error {
+	cfg, err := h.GetDefaultConfig()
+	if err != nil {
+		return err
+	}
+
+	if cfg != nil {
+		idxs := cfg.Indexers
+		e := reflect.ValueOf(&idxs).Elem()
+		for i := 0; i < e.NumField(); i++ {
+			idxConfig := e.Field(i).Interface()
+			if idxConfig != nil {
+				err := OpenIndexerConnection(idxConfig.(config.Indexer), h)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
 
 // OpenIndexerConnection open indexer connection
 func OpenIndexerConnection(config config.Indexer, h *handler.Handler) error {
--- system/initialize/indexer_test.go
+++ system/initialize/indexer_test.go
@@ -1,8 +1,11 @@
 package initialize
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
+	"github.com/d3ta-go/system/system/utils"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -14,4 +17,28 @@ func TestOpenAllIndexerConnection(t *testing.T) {
 		}
 	}
 
+	if assert.NoError(t, OpenAllIndexerConnection(h), "Error while opening all indexer connection: OpenAllIndexerConnection") {
+
+		cfg, err := h.GetDefaultConfig()
+		if !assert.NoError(t, err, "Error while getting config: h.GetConfig") {
+			return
+		}
+
+		idxCon, err := h.GetIndexer(cfg.Indexers.DataIndexer.ConnectionName)
+		if assert.NoError(t, err, "Error while getting Indexer Engine Connection: h.GetIndexer(cfg.Indexers.DataIndexer.ConnectionName)") {
+
+			idxCon.Context = "system"
+			idxCon.Container = "indexer"
+			idxCon.Component = "test"
+			index := fmt.Sprintf("test-index-%s", utils.GenerateUUID())
+
+			if assert.NoError(t, idxCon.CreateDoc(index, "test-indexer-index-id", strings.NewReader(`{ "test": "test value on initialize.indexer"}`))) {
+				res, err := idxCon.GetDoc(index, "test-indexer-index-id")
+				assert.NoError(t, err)
+				assert.NotNil(t, res)
+			}
+
+			assert.NoError(t, idxCon.DropIndex([]string{index}))
+		}
+	}
 }
