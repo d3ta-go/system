05634757cef2a9ea22265242282f7ffd5e69a975
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-11-08 08:08:21 +0700

    system.config: add field env and related method

--- system/config/default.go
+++ system/config/default.go
@@ -1,7 +1,10 @@
 package config
 
+import "encoding/json"
+
 // Config represent Config (Example)
 type Config struct {
+	Environment  Environment  `json:"environment" yaml:"environment"`
 	Applications Applications `json:"applications" yaml:"applications"`
 	IAM          IAM          `json:"IAM" yaml:"IAM"`
 	Securities   Securities   `json:"securities" yaml:"securities"`
@@ -11,4 +14,25 @@ type Config struct {
 	SMTPServers  SMTPServers  `json:"SMTPServers" yaml:"SMTPServers"`
 	Caches       Caches       `json:"caches" yaml:"caches"`
 	Indexers     Indexers     `json:"indexers" yaml:"indexers"`
+}
+
+// ToJSON convert Config to JSON
+func (c *Config) ToJSON() []byte {
+	JSON, err := json.Marshal(c)
+	if err != nil {
+		return nil
+	}
+	return JSON
+}
+
+// CanRunTest can Run Test
+func (c *Config) CanRunTest() bool {
+	can := false
+	for _, v := range c.Environment.RunTestEnvironment {
+		if string(v) == c.Environment.Stage {
+			can = true
+			break
+		}
+	}
+	return can
 }
